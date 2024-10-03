e9688a11ed32071b765e9a80be0a3c3a8cd83f77
Krishna Yadav <krishnayadav@MacBook-Pro-3.local>
2024-10-03 09:38:13 +0530

    ss

--- summarize_desc1.py
+++ summarize_desc1.py
@@ -16,13 +16,13 @@ class Summarization:
 
         
             llm = Ollama(model=self.model, temperature=0.3)
-            # if len(description.split(' ')) >= 100:
-            #     prompt = f"summarize the sentiment of following text. remove any integer value or web page link and any other noise and limit the output in between 30-60 words: {description}"
+            if len(description.split(' ')) >= 100:
+                prompt = f"summarize the sentiment of following text. remove any integer value or web page link and any other noise and limit the output in between 50-100 words: {description}"
             
-            # if len(description.split(' ')) < 100:
-            #     prompt = f"summarize the sentiment of following text. remove any integer value or web page link and any other noise and limit the output in between 10-20 words: {description}"
+            if len(description.split(' ')) < 100:
+                prompt = f"summarize the sentiment of following text. remove any integer value or web page link and any other noise and limit the output in between 10-30 words: {description}"
             
-            prompt = f"give me the positive or negative sentiment of the following text: {description}"
+            # prompt = f"give me the positive or negative sentiment of the following text: {description}"
             
             output = llm.invoke(prompt)
             print("\n\n", coin)
@@ -34,7 +34,7 @@ class Summarization:
             
             # Printing prompt in red and output in green on separate lines
             print(f"\n{RED}PROMPT:{RESET} {RED}{prompt}{RESET}")
-            print(f"\n{GREEN}OUTPUT:{RESET} {GREEN}{output}{RESET}")
+            print(f"\n{GREEN}OUTPUT:{RESET} train_3_15{GREEN}{output}{RESET}")
             
             post_df.at[index, 'proposal'] = output  # Update the description column with the LLM output
 
@@ -42,16 +42,16 @@ class Summarization:
         
         return post_df
     
-    def get_summary(self, post_df_dict):
+    def get_summary(self, post_df_dict, temp_llm):
         for coin, post_df in post_df_dict.items():
             filtered_descriptions = self.summarize_data(post_df, coin)
 
-            filtered_descriptions.to_csv(f"/Users/krishnayadav/Downloads/coin_data/summarized_data/{coin}.csv")
+            filtered_descriptions.to_csv(f"/Users/krishnayadav/Downloads/coin_data/summarized_data/{temp_llm}.csv")
             
 
 def read_files(post_directory):
     
-    # post_df_dict = {}
+    post_df_dict = {}
     # for post in os.listdir(post_directory):
     #     if post == '.DS_Store':
     #         continue
@@ -66,13 +66,16 @@ def read_files(post_directory):
     return post_df_dict
     
 if __name__ == "__main__":
-    summarize = Summarization("mistral")
-    post_directory = '/Users/krishnayadav/Downloads/ML_train/train_3_15.csv'
-    post_df_dict = read_files(post_directory)
-    summarize.get_summary(post_df_dict)
+    llm_list = ["mistral", "phi3", "llama2-uncensored"]
+    for temp_llm in llm_list:
+        print(temp_llm)
+        summarize = Summarization(temp_llm)
+        post_directory = '/Users/krishnayadav/Downloads/ML_train/train_3_15.csv'
+        post_df_dict = read_files(post_directory)
+        summarize.get_summary(post_df_dict, temp_llm)
     
-    
-    
+
+
 
 
 
